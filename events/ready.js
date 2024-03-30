07ee71cc48941f566b4dbeeb8821540537e0b11c
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-03-30 23:09:51 +0100

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const client = require("../index");
 client.on('ready', () => {
 console.log(`I'am ${client.user.tag} and nigger.`)
   client.user.setStatus("idle")
-    client.user.setActivity(`Your Mom`, { type: 'WATCHING' })
+    client.user.setActivity(`Aesthetical`, { type: 'WATCHING' })
 });
