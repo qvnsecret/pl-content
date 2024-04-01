04df4ed5740c4031e8e00e39b61cab2ded5abce7
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-01 12:37:43 +0200

    Create uptime.js

---
+++ commands/owner/uptime.js
@@ -0,0 +1,19 @@
+// uptime.js
+const { MessageEmbed } = require("discord.js");
+const { formatUptime } = require('./utils');
+
+module.exports = {
+    name: "uptime",
+    description: 'Get the bot\'s uptime',
+    run: async (client, message, args) => {
+        const uptime = formatUptime(client.uptime);
+
+        const embed = new MessageEmbed()
+            .setColor(message.guild.me.displayHexColor)
+            .setTitle('Bot Uptime')
+            .setDescription(`The bot has been up for: `/`/`/`${uptime}/`/`/``)
+            .setTimestamp();
+
+        message.reply({ embeds: [embed] });
+    },
+};
