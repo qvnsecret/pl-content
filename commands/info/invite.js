b066af3b65d04b11aed7407041f67643a4bbb8d9
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-18 19:54:19 +0200

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -16,7 +16,7 @@ module.exports = {
   .setURL(`https://discord.com/oauth2/authorize?client_id=1223343717184835754`))
 
     let embed = new MessageEmbed()
-.setTitle(`Social Media`) 
+.setTitle(`Support & Invite`) 
 .setDescription(`This bot was made by \`ogqvnrvx\`. This bot isn't made by the offical roblox Aesthetical. This bot was made for educational purposes only. We have a favor for you, [invite](https://discord.com/oauth2/authorize?client_id=1223343717184835754) and join our [Support Server](https://discord.gg/637NNhhS8n) :)`)     
 .setColor(`#2b2d31`)
 
