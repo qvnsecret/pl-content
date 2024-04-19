cb220b84fef7ac54a6f3cd3043fe1c153be3cf2d
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-20 00:38:57 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,4 +1,4 @@
-const { MessageButton, MessageActionRow, MessageEmbed, Client, MessageSelectMenu } = require("discord.js");
+const { MessageButton, MessageActionRow, MessageEmbed, MessageSelectMenu } = require("discord.js");
 const { glob } = require("glob");
 const { promisify } = require("util");
 
@@ -6,119 +6,74 @@ module.exports = {
     name: "help",
     description: 'Feeling lost?',
     aliases: [],
-    run: async (client,message, args) => {
-        const globPromise = promisify(glob);
-        const adminFiles = await globPromise(`${process.cwd()}/commands/admin/**/*.js`);   
-        const infoFiles = await globPromise(`${process.cwd()}/commands/info/**/*.js`);   
-        const ownerFiles = await globPromise(`${process.cwd()}/commands/owner/**/*.js`);  
-      
-       let menu = new MessageSelectMenu()
-      .setCustomId(`help_${message.author.id}`)
-      .setPlaceholder("Choose a category")
-         
-      .addOptions([
-						{
-							label: 'Info',
-							description: 'To view the info commands',
-							value: 'info',
-              emoji: '<:info:1220474955687399504>',
-						},
-            {
-							label: 'Owner',
-							description: 'To view the owner commands',
-							value: 'owner',
-              emoji: '<:owner:1220474945633652826>',
-            },
-            {
-							label: 'Admin',
-							description: 'To view the admin commands',
-							value: 'admin',
-              emoji: '<:employee:1220475795412090891>',
-            }])
-    
-   let row = new MessageActionRow()
-      .addComponents([menu]);
-  let button = new MessageActionRow()
+    run: async (client, message, args) => {
+        try {
+            const { prefix } = client.config;
 
-        .addComponents(
-            new MessageButton()
-  .setStyle('LINK')
-  .setLabel('Bug Server')
-  .setURL(`https://discord.gg/n2DR6VJtgP`))
-    
-       .addComponents(
-            new MessageButton()
-  .setStyle('LINK')
-  .setLabel('Server Support')
-  .setURL(`https://discord.com/invite/886Z9JUdXY`))
-    let embed = new MessageEmbed()    
+            const globPromise = promisify(glob);
+            const commandFiles = {
+                admin: await globPromise(`${process.cwd()}/commands/admin/**/*.js`),
+                info: await globPromise(`${process.cwd()}/commands/info/**/*.js`),
+                owner: await globPromise(`${process.cwd()}/commands/owner/**/*.js`),
+            };
 
-    .setTitle(`**Mytho**`)
-    .setDescription(`**Thank you for choosing our bot, <:mytho:1230996358081544222> \`Mytho\`. Below, you\'ll find a comprehensive list of all available commands for your convenience.**
-    Bot Prefix: ${client.config.prefix}help`)
-	                .addFields(
-                { name: '# Info', value: `edeedededed`, inline: true };
-            )
-    .setThumbnail(`https://cdn.discordapp.com/attachments/1223568250810404925/1223568278719561769/noFilter.png?ex=661a53c2&is=6607dec2&hm=85090d587e60078ca67e9b5750ea35688773b494e4fa9de2e317dd52f6dc4181&`)
-	    .setImage('https://cdn.discordapp.com/attachments/1218607459925753969/1229064062051291297/discordstatus_4.png?ex=662e521b&is=661bdd1b&hm=72f4b2365b4f0330643d98517e1f30e9cc85a7c068e1c73848fd740736f60a8c&')
-      .setFooter(`This bot isn't made by the offical roblox Aesthetical. This bot was made for educational purposes only.`)
+            const menuOptions = [
+                { label: 'Info', description: 'To view the info commands', value: 'info', emoji: '<:info:1220474955687399504>' },
+                { label: 'Owner', description: 'To view the owner commands', value: 'owner', emoji: '<:owner:1220474945633652826>' },
+                { label: 'Admin', description: 'To view the admin commands', value: 'admin', emoji: '<:employee:1220475795412090891>' }
+            ];
 
-.setColor(`#2b2d31`)
-      .setTimestamp()
-    message.reply({ embeds:[embed], components:[row, button] }).then( msg => {
-      let filter = b => b.user.id === message.author.id && b.customId === `help_${message.author.id}`;
-      let collector = msg.createMessageComponentCollector({ filter:filter, componentType: 'SELECT_MENU', time:120000 });
-      collector.on("collect", (b) => {
-      if(b.values[0] === "admin") {   
-      let embed_1 = new MessageEmbed()
-       .setColor(`#2b2d31`)
-      .setTimestamp()
-          
-    adminFiles.map((value) => {
-        const file = require(value);
-        const splitted = value.split("/");
-        const directory = splitted[splitted.length - 2];
+            const menu = new MessageSelectMenu()
+                .setCustomId(`help_${message.author.id}`)
+                .setPlaceholder("Choose a category")
+                .addOptions(menuOptions);
 
-        if (file.name) {
-            const properties = { directory, ...file };
-            embed_1.addField(`${client.config.prefix}${properties.name}`, `${properties.description}`, true)
+            const row = new MessageActionRow().addComponents(menu);
+
+            const buttonRow = new MessageActionRow()
+                .addComponents(
+                    new MessageButton().setStyle('LINK').setLabel('Bug Server').setURL(`https://discord.gg/n2DR6VJtgP`),
+                    new MessageButton().setStyle('LINK').setLabel('Server Support').setURL(`https://discord.com/invite/886Z9JUdXY`)
+                );
+
+            const embed = new MessageEmbed()
+                .setTitle('**Mytho**')
+                .setDescription(`**Thank you for choosing our bot, <:mytho:1230996358081544222> \`Mytho\`. Below, youll find a comprehensive list of all available commands for your convenience.**\nBot Prefix: ${prefix}help`)
+                .setThumbnail('https://cdn.discordapp.com/attachments/1223568250810404925/1223568278719561769/noFilter.png?ex=661a53c2&is=6607dec2&hm=85090d587e60078ca67e9b5750ea35688773b494e4fa9de2e317dd52f6dc4181&')
+                .setImage('https://cdn.discordapp.com/attachments/1218607459925753969/1229064062051291297/discordstatus_4.png?ex=662e521b&is=661bdd1b&hm=72f4b2365b4f0330643d98517e1f30e9cc85a7c068e1c73848fd740736f60a8c&')
+                .setFooter("This bot isn't made by the official roblox Aesthetical. This bot was made for educational purposes only.")
+                .setColor('#2b2d31')
+                .setTimestamp();
+
+            const collectorFilter = (b) => b.user.id === message.author.id && b.customId === `help_${message.author.id}`;
+            const collector = message.reply({ embeds: [embed], components: [row, buttonRow] }).then(msg => {
+                const componentCollector = msg.createMessageComponentCollector({ filter: collectorFilter, componentType: 'SELECT_MENU', time: 120000 });
+
+                componentCollector.on("collect", async (b) => {
+                    let embedUpdate = new MessageEmbed().setColor('#2b2d31').setTimestamp();
+
+                    switch (b.values[0]) {
+                        case "admin":
+                        case "info":
+                        case "owner":
+                            const category = b.values[0];
+                            commandFiles[category].map((value) => {
+                                const file = require(value);
+                                const splitted = value.split("/");
+                                const directory = splitted[splitted.length - 2];
+                                if (file.name) {
+                                    const properties = { directory, ...file };
+                                    embedUpdate.addField(`${prefix}${properties.name}`, `${properties.description}`, true);
+                                }
+                            });
+                            break;
+                    }
+                    await b.update({ embeds: [embedUpdate], components: [row, buttonRow] }).catch(console.error);
+                });
+            });
+        } catch (error) {
+            console.error(error);
+            message.reply("An error occurred while processing your request.");
         }
-    })
-          b.update({ embeds:[embed_1], components:[row, button] }).catch(err => {});
-        } else if(b.values[0] === "info") {
-      let embed_1 = new MessageEmbed()
-       .setColor(`#2b2d31`)
-      .setTimestamp()
-                 
-    infoFiles.map((value) => {
-        const file = require(value);
-        const splitted = value.split("/");
-        const directory = splitted[splitted.length - 2];
-
-        if (file.name) {
-            const properties = { directory, ...file };
-            embed_1.addField(`${client.config.prefix}${properties.name}`, `${properties.description}`, true)
-        }
-    })
-          b.update({ embeds:[embed_1], components:[row, button] }).catch(err => {});
-        } else if(b.values[0] === "owner")
-  {
-let embed_1 = new MessageEmbed()
-       .setColor(`#2b2d31`)
-      .setTimestamp()
-                 
-    ownerFiles.map((value) => {
-        const file = require(value);
-        const splitted = value.split("/");
-        const directory = splitted[splitted.length - 2];
-
-        if (file.name) {
-            const properties = { directory, ...file };
-            embed_1.addField(`${client.config.prefix}${properties.name}`, `${properties.description}`, true)
-        }
-    })
-          b.update({ embeds:[embed_1], components:[row, button] }).catch(err => {});  }
-      });
-    });
-   },
+    },
 };
