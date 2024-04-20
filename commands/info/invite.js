a3b2519575ae79b524aa11b8a54acf93a2853365
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-20 23:17:43 +0200

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -1,34 +1,27 @@
-const { MessageButton, MessageActionRow, MessageEmbed, Client, MessageSelectMenu } = require("discord.js");
-const { glob } = require("glob");
-const { promisify } = require("util");
+const { MessageButton, MessageActionRow, MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "invite",
     description: 'Invite & Support our bot. You will help us a lot.',
     aliases: [],
-    run: async (client,message, args) => {
-
-  let button = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-  .setStyle('LINK')
-  .setLabel('Invite the Bot')
-  .setURL(`https://discord.com/oauth2/authorize?client_id=1223343717184835754`))
-
+    run: async (client, message, args) => {
+        let button = new MessageActionRow()
             .addComponents(
-            new MessageButton()
-  .setStyle('LINK')
-  .setLabel('Bug Server')
-  .setURL(`https://discord.gg/5enJVWgY4m`))
+                new MessageButton()
+                    .setStyle('LINK')
+                    .setLabel('Invite the Bot')
+                    .setURL('https://discord.com/oauth2/authorize?client_id=1223343717184835754')
+            );
 
-    let embed = new MessageEmbed()
-.setTitle(`Support & Invite`) 
-.setDescription(`If you really wanna support us and [invite](https://discord.com/oauth2/authorize?client_id=1223343717184835754) our bot to reach our goal, we appreciate it alot.
-We are currently at \`${guilds.size}\` guilds!`)  
-.setColor(`#2b2d31`)
+        // To correctly display the number of guilds, you must fetch this from the client object
+        let guildCount = client.guilds.cache.size;
 
-      .setTimestamp()
+        let embed = new MessageEmbed()
+            .setTitle('Support & Invite')
+            .setDescription(`If you really wanna support us and [invite](https://discord.com/oauth2/authorize?client_id=1223343717184835754) our bot to reach our goal, we appreciate it a lot.\nWe are currently at \`${guildCount}\` guilds!`)
+            .setColor('#2b2d31')
+            .setTimestamp();
 
-    message.reply({ embeds:[embed], components:[button] })
-          },
+        message.reply({ embeds: [embed], components: [button] });
+    },
 };
