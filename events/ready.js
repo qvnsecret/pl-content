653c660d4a5866cbebd433d11abfd414745d9a0d
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-20 12:10:25 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const client = require("../index");
 
 client.on('ready', () => {
-console.log(`I'am ${client.user.tag} and nigger.`)
+console.log(`I'am ${client.user.tag}, the best bot & app in the world :skull:`)
   client.user.setStatus("idle")
-    client.user.setActivity(`Aesthetical`, { type: 'WATCHING' })
+    client.user.setActivity(`Guilds: ${guilds.size}`, { type: 'LISTENING' })
 });
