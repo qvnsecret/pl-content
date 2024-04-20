badea59d13cdba11cf1d037129c847968e3cdd7e
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-20 17:37:45 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const client = require("../index");
 client.on('ready', () => {
 console.log(`I'am ${client.user.tag}, the best bot & app in the world :skull:`)
   client.user.setStatus("idle")
-    client.user.setActivity(`Over 5 Servers`, { type: 'LISTENING' })
+    client.user.setActivity(`& PROTECTING`, { type: 'WATCHING' })
 });
