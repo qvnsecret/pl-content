0ddf6e15b1991bac0beca46537f93def0045102b
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-21 17:08:28 +0200

    Update addemoji.js

--- commands/admin/addemoji.js
+++ commands/admin/addemoji.js
@@ -1,35 +1,50 @@
-const { Message, Client, Util } = require("discord.js");
+const { MessageEmbed, Util } = require("discord.js");
 
 module.exports = {
-        name: "addemoji",
-        description: `add emojis to the server(can add multiple emojis at the same time).`,
-        run: async (client, message, args) => {
-                const permission = message.member.permissions.has("MANAGE_EMOJIS_AND_STICKERS ");
-                const guilds = message.guild.me.permissions.has("MANAGE_EMOJIS_AND_STICKERS ");
-                if (!permission) return message.reply({ content: ":rolling_eyes: **You don't have permission**" });
-                let emojis = args.join(' ').match(/<?(a)?:?(\w{2,32}):(\d{17,19})>?/gi);
-                if (!emojis) return message.reply({ content: `:rolling_eyes: **Please provide emojis to add.**` }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
-                let emojisA = []
-                emojis.forEach((emote) => {
-                        let emoji = Util.parseEmoji(emote);
-                        if (emoji.id) {
-                                const Link = `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.animated ? "gif" : "png"}`;
-                                message.guild.emojis.create(`${Link}`, `${emoji.name}`).then((em) => {
-                                        emojisA.push(em.toString())
-                                        if (emojis.length == emojisA.length) {
-                                                message.reply(`${emojisA.map(e => e).join(',')} **Done added emoji**`).catch((err) => {
-                                                        console.log(`i couldn't reply to the message: ` + err.message)
-                                                })
-                                                emojisA = []
-                                        }
-                                }).catch((error) => {
-                                        message.reply("Error : " + error.message).catch((err) => {
-                                                console.log(`i couldn't reply to the message: ` + err.message)
-                                        })
-                                });
-                        }
-                })
-        },
+    name: "addemoji",
+    description: "Add emojis to the server (can add multiple emojis at the same time).",
+    run: async (client, message, args) => {
+        const permission = message.member.permissions.has("MANAGE_EMOJIS_AND_STICKERS");
+        if (!permission) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**You don't have permission >:(**");
+            return message.reply({ embeds: [embed] });
+        }
+
+        let emojis = args.join(' ').match(/<?(a)?:?(\w{2,32}):(\d{17,19})>?/gi);
+        if (!emojis) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**Please provide emojis to add.**");
+            return message.reply({ embeds: [embed] });
+        }
+
+        let emojisA = [];
+        emojis.forEach((emote) => {
+            let emoji = Util.parseEmoji(emote);
+            if (emoji.id) {
+                const Link = `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.animated ? "gif" : "png"}`;
+                message.guild.emojis.create(`${Link}`, `${emoji.name}`).then((em) => {
+                    emojisA.push(em.toString())
+                    if (emojis.length == emojisA.length) {
+                        const embed = new MessageEmbed()
+                            .setColor("#2b2d31")
+                            .setDescription(`${emojisA.map(e => e).join(',')} **Done added emoji**`);
+                        message.reply({ embeds: [embed] }).catch((err) => {
+                            console.log(`I couldn't reply to the message: ` + err.message);
+                        });
+                        emojisA = [];
+                    }
+                }).catch((error) => {
+                    const embed = new MessageEmbed()
+                        .setColor("#2b2d31")
+                        .setDescription("Error: " + error.message);
+                    message.reply({ embeds: [embed] }).catch((err) => {
+                        console.log(`I couldn't reply to the message: ` + err.message);
+                    });
+                });
+            }
+        });
+    },
 };
