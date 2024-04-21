1b89d5530be9a3bf81027fcd0b8b616236c8e721
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-21 17:42:54 +0200

    Update lock.js

--- commands/admin/lock.js
+++ commands/admin/lock.js
@@ -1,29 +1,39 @@
-const { Message, Client } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-        name: "lock",
-        description: `Disables everyone from sending messages in specific channel.`,
-        run: async (client, message, args) => {
-                const permission = message.member.permissions.has("MANAGE_CHANNELS");
-                const guilds = message.guild.me.permissions.has("MANAGE_CHANNELS");
+    name: "lock",
+    description: "Disables everyone from sending messages in a specific channel.",
+    run: async (client, message, args) => {
+        const permission = message.member.permissions.has("MANAGE_CHANNELS");
+        const botPermission = message.guild.me.permissions.has("MANAGE_CHANNELS");
 
-                if (!permission)
-                        return message.reply(
-                                { content: ":x: **You don't have permission to use this command**", ephemeral: true }
-                        ).catch((err) => {
-                                console.log(`i couldn't reply to the message: ` + err.message)
-                        })
+        if (!permission) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**You don't have permission to use this command**");
+            return message.reply({ embeds: [embed], ephemeral: true }).catch(err => console.log(`I couldn't reply to the message: ${err.message}`));
+        }
 
-                if (!guilds) return message.reply({ content: `:rolling_eyes: **I couldn't edit the channel permissions. Please check my permissions and role position.**`, ephemeral: true }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
-                let everyone = message.guild.roles.cache.find(hyper => hyper.name === '@everyone');
-                message.channel.permissionOverwrites.edit(everyone, {
-                        SEND_MESSAGES: false
-                }).then(() => {
-                        message.reply({ content: `:lock: **${message.channel.name} has been looked.** `, ephemeral: true }).catch((err) => {
-                                console.log(`i couldn't reply to the message: ` + err.message)
-                        })
-                })
-        },
+        if (!botPermission) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**I couldn't edit the channel permissions. Please check my permissions and role position.**");
+            return message.reply({ embeds: [embed], ephemeral: true }).catch(err => console.log(`I couldn't reply to the message: ${err.message}`));
+        }
+
+        let everyone = message.guild.roles.cache.find(role => role.name === '@everyone');
+        message.channel.permissionOverwrites.edit(everyone, {
+            SEND_MESSAGES: false
+        }).then(() => {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription(`**${message.channel.name} has been locked.**`);
+            message.reply({ embeds: [embed], ephemeral: true }).catch(err => console.log(`I couldn't reply to the message: ${err.message}`));
+        }).catch(err => {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription(`**Failed to lock the channel: ${err.message}**`);
+            message.reply({ embeds: [embed], ephemeral: true }).catch(error => console.log(`I couldn't reply to the message: ${error.message}`));
+        });
+    },
 };
