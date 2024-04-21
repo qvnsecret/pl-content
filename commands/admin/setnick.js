d6546a60c6b39898d0e6199f575a242897818646
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-21 20:09:22 +0200

    Update setnick.js

--- commands/admin/setnick.js
+++ commands/admin/setnick.js
@@ -1,39 +1,60 @@
-const { Message, Client } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-        name: "setnick",
-        description: `to change the member nickname.`,
-        run: async (client, message, args) => {
-                const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    name: "setnick",
+    description: "Changes the member's nickname.",
+    run: async (client, message, args) => {
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        const permission = message.member.permissions.has("MANAGE_NICKNAMES");
+        const botPermission = message.guild.me.permissions.has("MANAGE_NICKNAMES");
 
-                const permission = message.member.permissions.has("MANAGE_NICKNAMES");
-                const guilds = message.guild.me.permissions.has("MANAGE_NICKNAMES");
-                if (!permission) return message.reply({ content: ":x: **You don't have permission to use this command**" }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
+        if (!permission) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**You don't have permission to use this command**");
+            return message.reply({ embeds: [embed] }).catch(err => console.log(`I couldn't reply to the message: ${err.message}`));
+        }
 
-                if (!args[0]) return message.reply({ content: `:rolling_eyes: **Please mention member or id**` }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
+        if (!args[0]) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**Please mention a member or provide their ID**");
+            return message.reply({ embeds: [embed] });
+        }
 
-                if (!member) return message.reply({ content: `:rolling_eyes: **I can't find this member**` }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
-                if (message.member.roles.highest.position < member.roles.highest.position) return message.reply({ content: `:rolling_eyes: **${member.user.username} have higher role than you**` })
-          if (!guilds) return message.reply({ content: `:rolling_eyes: **I couldn't timeout that user. Please check my permissions and role position.**` }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
-let name = args.slice(1).join(' ')
-          if(!name){
-            member.setNickname(` `).then(() => {
-              message.reply(`:white_check_mark: **${member.user.username}** nickname has been reset`)
-            })
-          }else{
-            member.setNickname(`${name}`).then(() => {
-              message.reply(`:white_check_mark: **${member.user.username}** nickname has been changed to **${name}**`)
-            }).catch((err) => {
-          console.log(`i couldn't reply to the message: ` + err.message)
-                })
-          }
-        },
+        if (!member) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**I can't find this member**");
+            return message.reply({ embeds: [embed] });
+        }
+
+        if (!botPermission) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription("**I don't have the necessary permissions to change nicknames. Please check my role position and permissions.**");
+            return message.reply({ embeds: [embed] });
+        }
+
+        if (message.member.roles.highest.position <= member.roles.highest.position && message.member.id !== message.guild.ownerID) {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription(`**You cannot change the nickname of ${member.user.username} as they have a higher or equal role than you.**`);
+            return message.reply({ embeds: [embed] });
+        }
+
+        let name = args.slice(1).join(' ') || null;
+        member.setNickname(name).then(() => {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription(`**${member.user.username}'s** nickname has been ${name ? `changed to **${name}**` : "reset"}`);
+            message.reply({ embeds: [embed] });
+        }).catch(err => {
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription(`**Failed to change nickname for ${member.user.username}: ${err.message}**`);
+            message.reply({ embeds: [embed] });
+            console.log(`I couldn't change the nickname: ${err.message}`);
+        });
+    },
 };
