d4e9b8d4e86c58387f210dcf04ab9f7c02716313
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-22 11:36:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,43 @@ const app = express();
 const chalk = require("chalk");
 
 app.get('/', (req, res) => {
-        res.send('Hello Express app!')
-})
+    res.send('Hello Express app!')
+});
+
 app.use('/ping', (req, res) => {
-        res.send(new Date());
+    res.send(new Date());
 });
+
 app.listen(3000, () => {
-        console.log(chalk.greenBright.bold('Listen to me carefully, your fuckng bot is already ready fucking ready, so go kys nig... jk 💀'))
+    console.log(chalk.greenBright.bold('The server is now running and listening on port 3000.'));
 });
 
-const { Client, Collection } = require("discord.js");
-
+const { Client, Collection, MessageEmbed } = require("discord.js");
 const client = new Client({
-        intents: 32767,
-        allowedMentions: { repliedUser: false },
+    intents: 32767,
+    allowedMentions: { parse: ['users', 'roles'], repliedUser: false },
 });
+
 module.exports = client;
 client.commands = new Collection();
 client.slashCommands = new Collection();
 client.config = require("./config.json");
+
 require("./handler")(client);
 
-client.login(process.env.token || ((_a = config.json) === null || _a === void 0 ? void 0 : _a.token)).catch((err) => {
-   console.log(err.message)
+client.on('messageCreate', message => {
+    // Check if the bot was mentioned using its ID and if the message was not sent by a bot
+    if (message.mentions.has(client.user.id) && !message.author.bot) {
+        const embed = new MessageEmbed()
+            .setColor("#2b2d31")  // Dark gray color
+            .setDescription("My prefix is `.`\nIf you want more info, type `.help` to see all the commands available.")
+            .setThumbnail(client.user.displayAvatarURL())  // Optional: Displays bot's avatar
+            .setTimestamp();
+
+        message.reply({ embeds: [embed] });
+    }
+});
+
+client.login(process.env.token || client.config.token).catch(err => {
+    console.error(err.message);
 });
