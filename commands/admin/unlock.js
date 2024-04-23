9438bbaf61dc5f04cd832ecf09beb96e87c2a0d5
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-23 19:46:35 +0200

    Update unlock.js

--- commands/admin/unlock.js
+++ commands/admin/unlock.js
@@ -1,27 +1,53 @@
-const { Message, Client } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-        name: "unlock",
-        description: `Allows everyone to send messages in specific channel.`,
-        run: async (client, message, args) => {
-                const permission = message.member.permissions.has("MANAGE_CHANNELS");
-                const guilds = message.guild.me.permissions.has("MANAGE_CHANNELS");
+    name: "unlock",
+    description: "Allows everyone to send messages in the specified channel.",
+    run: async (client, message, args) => {
+        // Check if the member has the required permission to manage channels
+        if (!message.member.permissions.has("MANAGE_CHANNELS")) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("You do not have permission to unlock channels.")]
+            }).catch(console.error);
+        }
 
-                if (!permission)
-                        return message.reply(
-                                { content: ":x: **You don't have permission to use this command**", ephemeral: true }
-                        ).catch((err) => {
-                                console.log(`i couldn't reply to the message: ` + err.message)
-                        })
+        // Check if the bot has the required permission to manage channels
+        if (!message.guild.me.permissions.has("MANAGE_CHANNELS")) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("I do not have permission to change channel settings.")]
+            }).catch(console.error);
+        }
 
-                if (!guilds) return message.reply({ content: `:rolling_eyes: **I couldn't change the channel permissions. Please check my permissions.**`, ephemeral: true }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
-                let everyone = message.guild.roles.cache.find(hyper => hyper.name === '@everyone');
-                message.channel.permissionOverwrites.edit(everyone, {
-                        SEND_MESSAGES: true
-                }).then(() => {
-                        message.reply({ content: `:unlock: **${message.channel.name} has been unlocked.**`, ephemeral: true })
-                })
-        },
+        // Find the @everyone role by comparing role names directly
+        const everyoneRole = message.guild.roles.cache.find(role => role.name === '@everyone');
+        if (!everyoneRole) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("Failed to find the '@everyone' role.")]
+            }).catch(console.error);
+        }
+
+        // Edit permissions for the @everyone role to allow sending messages
+        message.channel.permissionOverwrites.edit(everyoneRole, {
+            SEND_MESSAGES: true
+        }).then(() => {
+            message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription(`${message.channel.name} has been unlocked and everyone can send messages now.`)]
+            }).catch(console.error);
+        }).catch(err => {
+            console.error(err);
+            message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("There was an error while trying to unlock the channel.")]
+            }).catch(console.error);
+        });
+    },
 };
