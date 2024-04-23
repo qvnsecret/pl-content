002d8da66e5c8c2d4b3725487ab750f8d36b5b51
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-23 20:57:01 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = {
     name: "help",
@@ -11,6 +11,50 @@ module.exports = {
             .setFooter("Mytho™ - 2024/2025")
             .setTimestamp();
 
-        message.channel.send({ embeds: [embed] });
+        const buttons = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('privacy')
+                    .setLabel('Privacy')
+                    .setStyle('SECONDARY'),
+                new MessageButton()
+                    .setCustomId('tou')
+                    .setLabel('Terms of Use')
+                    .setStyle('SECONDARY'),
+                new MessageButton()
+                    .setCustomId('all_commands')
+                    .setLabel('All Commands')
+                    .setStyle('PRIMARY')
+            );
+
+        message.channel.send({ embeds: [embed], components: [buttons] });
+
+        const filter = i => i.user.id === message.author.id;
+        const collector = message.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'privacy') {
+                const privacyEmbed = new MessageEmbed()
+                    .setTitle("Privacy Policy")
+                    .setDescription("Here's the detailed [Privacy Policy](https://qvnsecret.github.io/mytho/)")
+                    .setColor("#2b2d31");
+                await i.reply({ embeds: [privacyEmbed], ephemeral: true });
+            } else if (i.customId === 'tou') {
+                const tosEmbed = new MessageEmbed()
+                    .setTitle("Terms of Use")
+                    .setDescription("Here's the detailed Terms of Use")
+                    .setColor("#2b2d31");
+                await i.reply({ embeds: [tosEmbed], ephemeral: true });
+            } else if (i.customId === 'all_commands') {
+                const commandsEmbed = new MessageEmbed()
+                    .setTitle("All Commands")
+                    .setDescription("Overview of all commands available.")
+                    .addField("Soon", "`Our team is working on it...`")
+                    .setColor("#2b2d31");
+                await i.reply({ embeds: [commandsEmbed], ephemeral: true });
+            }
+        });
+
+        collector.on('end', collected => console.log(`Collected ${collected.size} items`));
     },
 };
