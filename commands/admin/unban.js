f36821a705b40e84c7e5d29beffafcf1ed51f669
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-23 19:41:09 +0200

    Update unban.js

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -1,44 +1,48 @@
-const { Message, Client } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-        name: "unban",
-        description: `Unbans a memeber.`,
-        run: async (client, message, args) => {
-                const permission = message.member.permissions.has("BAN_MEMBERS");
-                const guilds = message.guild.me.permissions.has("BAN_MEMBERS");
+    name: "unban",
+    description: "Unbans a member from the server.",
+    run: async (client, message, args) => {
+        const permission = message.member.permissions.has("BAN_MEMBERS");
+        const botPermission = message.guild.me.permissions.has("BAN_MEMBERS");
 
-                if (!permission)
-                        return message.reply(
-                                { content: ":x: **You don't have permission to use this command**", ephemeral: true }
-                        ).catch((err) => {
-                                console.log(`i couldn't reply to the message: ` + err.message)
-                        })
+        if (!permission) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("You don't have permission to unban members.")]
+            }).catch(console.error);
+        }
 
-                if (!guilds) return message.reply({ content: `:rolling_eyes: **I couldn't unban that user. Please check my permissions and role position.**`, ephemeral: true }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
+        if (!botPermission) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("I don't have permission to unban members. Please check my role position.")]
+            }).catch(console.error);
+        }
 
-                let user = message.content.split(" ").slice(1).join(" ");
-                if (!user) return message.reply({
-                        content: `:rolling_eyes: **Please mention member or id**`,
-                        ephemeral: true
-                }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
-                message.guild.members.unban(user).then(m => {
-                        message.reply({
-                                content: `:white_check_mark: **${m.username} Unbanned!**`,
-                                ephemeral: true
-                        }).catch((err) => {
-                                console.log(`i couldn't reply to the message: ` + err.message)
-                        })
-                }).catch(err => {
-                        message.reply({
-                                content: `:rolling_eyes: **I can't find <${user}> in the ban list**`,
-                                ephemeral: true
-                        }).catch((err) => {
-                                console.log(`i couldn't reply to the message: ` + err.message)
-                        })
-                })
-        },
+        if (!args[0]) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("Please provide the ID of the member to unban.")]
+            }).catch(console.error);
+        }
+
+        message.guild.members.unban(args[0]).then(user => {
+            message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription(`${user.tag} has been unbanned successfully.`)]
+            });
+        }).catch(err => {
+            message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription("I can't find that user in the ban list or an error occurred.")]
+            }).catch(console.error);
+        });
+    },
 };
