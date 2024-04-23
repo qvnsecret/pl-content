45b5123b6abbcb5f3f1701775fb5d71212fab4e4
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-23 20:06:02 +0200

    Update warnings.js

--- commands/admin/warnings.js
+++ commands/admin/warnings.js
@@ -2,31 +2,51 @@ const { Message, Client, MessageEmbed } = require("discord.js");
 const db = require('quick.db')
 
 module.exports = {
-        name: "warnings",
-        description: `Get the list of warnings for the server or a user`,
-        run: async (client, message, args) => {
-                const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-                const permission = message.member.permissions.has("MANAGE_MESSAGES");
-          
-      if (!permission) return message.reply({ content: ":x: **You don't have permission to use this command**" }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
+    name: "warnings",
+    description: "Get the list of warnings for the server or a user",
+    run: async (client, message, args) => {
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        const permission = message.member.permissions.has("MANAGE_MESSAGES");
 
-                if (!args[0]) return message.reply({ content: `:rolling_eyes: **Please mention member or id**` }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
+        if (!permission) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription(":x: You don't have permission to use this command")]
+            }).catch((err) => {
+                console.log(`I couldn't reply to the message: ${err.message}`);
+            });
+        }
 
-                if (!member) return message.reply({ content: `:rolling_eyes: **I can't find this member**` }).catch((err) => {
-                        console.log(`i couldn't reply to the message: ` + err.message)
-                })
+        if (!args[0]) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription(":rolling_eyes: Please mention a member or provide their ID")]
+            }).catch((err) => {
+                console.log(`I couldn't reply to the message: ${err.message}`);
+            });
+        }
 
-     let warns = await db.fetch(`warns_${member.id}`)
+        if (!member) {
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor("#2b2d31")
+                    .setDescription(":rolling_eyes: I can't find this member")]
+            }).catch((err) => {
+                console.log(`I couldn't reply to the message: ${err.message}`);
+            });
+        }
 
-   if(!warns == null) warns = 0;
-          
-      let embed = new MessageEmbed()
-  .setDescription(`Warn ID (${warns}) by ${member}`)
-    message.reply({embeds: [embed] }) 
-          
-        },
+        let warns = await db.fetch(`warns_${member.id}`);
+        if (!warns) warns = 0;
+
+        let embed = new MessageEmbed()
+            .setColor("#2b2d31")
+            .setDescription(`Warn ID (${warns}) by ${member}`);
+
+        message.reply({ embeds: [embed] }).catch((err) => {
+            console.log(`I couldn't reply to the message: ${err.message}`);
+        });
+    },
 };
