ab3d62a3ab00b2484a6d859ef94a81faa868dca0
qvnsecret <155643316+qvnsecret@users.noreply.github.com>
2024-04-26 11:42:52 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args) => {
         const pingEmbed = new MessageEmbed()
             .setColor("#FF3A3A")
-            .setDescription(`⚡ Response time ${client.ws.ping} ms. For any bug report [here](https://discord.gg/dpxybg8ZbD)`);
+            .setDescription(`Response time ${client.ws.ping} ms. For any bug report [here](https://discord.gg/dpxybg8ZbD)`);
 
         message.reply({ embeds: [pingEmbed] });
     },
