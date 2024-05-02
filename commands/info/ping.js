f5a74cfcfd2c5d2b8ee6de57063ec966202a331f
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-02 16:22:16 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args) => {
         const pingEmbed = new MessageEmbed()
             .setColor("#FF3A3A")
-            .setDescription(`Response time ${client.ws.ping} ms. For any bug report [here](https://discord.gg/dpxybg8ZbD)`);
+            .setDescription(`<:ping:1233853207323611236> Response time ${client.ws.ping} ms. For any bug report [here](https://discord.gg/dpxybg8ZbD)`);
 
         message.reply({ embeds: [pingEmbed] });
     },
