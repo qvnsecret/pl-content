ab54b117f5fb7723347f6c66900737125a002b5a
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-02 22:28:12 +0200

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,5 +1,4 @@
-const { MessageButton, MessageActionRow, MessageEmbed } = require("discord.js");
-const axios = require('axios');
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = {
   name: "avatar",
@@ -8,66 +7,52 @@ module.exports = {
   run: async (client, message, args) => {
     const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
-    try {
-      const { data } = await axios.get(`https://discord.com/api/users/${Member.id}`, {
-        headers: {
-          Authorization: `Bot ${client.config.discord.token}`,
-        },
-      });
+    // Extract avatar URLs in different formats
+    const avatarURLs = {
+      png: Member.user.displayAvatarURL({ format: 'png', size: 4096 }),
+      webp: Member.user.displayAvatarURL({ format: 'webp', size: 4096 }),
+      jpeg: Member.user.displayAvatarURL({ format: 'jpeg', size: 4096 }),
+      gif: Member.user.displayAvatarURL({ format: 'gif', dynamic: true, size: 4096 }), // Only works if it is a GIF
+      jpg: Member.user.displayAvatarURL({ format: 'jpg', size: 4096 })
+    };
 
-      const avatarURLs = {
-        png: `https://cdn.discordapp.com/avatars/${Member.id}/${data.avatar}.png?size=4096`,
-        webp: `https://cdn.discordapp.com/avatars/${Member.id}/${data.avatar}.webp?size=4096`,
-        jpeg: `https://cdn.discordapp.com/avatars/${Member.id}/${data.avatar}.jpeg?size=4096`,
-        gif: `https://cdn.discordapp.com/avatars/${Member.id}/${data.avatar}.gif?size=4096`,
-        jpg: `https://cdn.discordapp.com/avatars/${Member.id}/${data.avatar}.jpg?size=4096`,
-      };
+    let embed = new MessageEmbed()
+      .setTimestamp()
+      .setTitle(`Avatar of ${Member.user.tag}`)
+      .setURL(Member.user.displayAvatarURL({ size: 4096, dynamic: true }))
+      .setColor("#2b2d31")
+      .setAuthor(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+      .setImage(avatarURLs.png)
+      .setFooter(`Avatar of ${Member.user.tag}`, Member.user.displayAvatarURL({ size: 4096, dynamic: true }));
 
-      const bannerData = data.banner? `https://cdn.discordapp.com/banners/${Member.id}/${data.banner}.${data.banner.startsWith('a_')? 'gif' : 'png'}?size=4096` : null;
+    let button1 = new MessageButton()
+      .setStyle('LINK')
+      .setLabel('Download PNG')
+      .setURL(avatarURLs.png);
 
-      let embed = new MessageEmbed()
-       .setTimestamp()
-       .setTitle(`Avatar of ${Member.user.tag}`)
-       .setURL(Member.user.displayAvatarURL({ size: 4096, dynamic: true }))
-       .setColor("#2b2d31")
-       .setAuthor(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-       .setFooter(`Avatar of ${Member.user.tag}`, Member.user.displayAvatarURL({ size: 4096, dynamic: true }));
+    let button2 = new MessageButton()
+      .setStyle('LINK')
+      .setLabel('Download WEBP')
+      .setURL(avatarURLs.webp);
 
-      if (bannerData) embed.setThumbnail(bannerData);
-      embed.setImage(avatarURLs.png);
+    let button3 = new MessageButton()
+      .setStyle('LINK')
+      .setLabel('Download JPEG')
+      .setURL(avatarURLs.jpeg);
 
-      let button1 = new MessageButton()
-       .setStyle('LINK')
-       .setLabel('Download PNG')
-       .setURL(avatarURLs.png);
+    let button4 = new MessageButton()
+      .setStyle('LINK')
+      .setLabel('Download GIF')
+      .setURL(avatarURLs.gif);
 
-      let button2 = new MessageButton()
-       .setStyle('LINK')
-       .setLabel('Download WEBP')
-       .setURL(avatarURLs.webp);
+    let button5 = new MessageButton()
+      .setStyle('LINK')
+      .setLabel('Download JPG')
+      .setURL(avatarURLs.jpg);
 
-      let button3 = new MessageButton()
-       .setStyle('LINK')
-       .setLabel('Download JPEG')
-       .setURL(avatarURLs.jpeg);
+    let row = new MessageActionRow()
+      .addComponents(button1, button2, button3, button4, button5);
 
-      let button4 = new MessageButton()
-       .setStyle('LINK')
-       .setLabel('Download GIF')
-       .setURL(avatarURLs.gif);
-
-      let button5 = new MessageButton()
-       .setStyle('LINK')
-       .setLabel('Download JPG')
-       .setURL(avatarURLs.jpg);
-
-      let row = new MessageActionRow()
-       .addComponents(button1, button2, button3, button4, button5);
-
-      message.reply({ embeds: [embed], components: [row] });
-    } catch (error) {
-      console.error(error);
-      message.reply('An error occurred while fetching the user data.');
-    }
+    message.reply({ embeds: [embed], components: [row] });
   },
 };
