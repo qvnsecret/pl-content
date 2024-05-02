6f8de0c75fc994e68d38cbd049c152f1339d6124
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-02 15:55:38 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ client.on('ready', () => {
     console.log(`I am ${client.user.tag}, the best bot & app in the world :skull:`);
     client.user.setStatus("online");
     const guildCount = client.guilds.cache.size;
-    client.user.setActivity(`Protecting ${guildCount} guilds`);
+    client.user.setActivity(`${guildCount} guilds`, { type: 'LISTENING' }); // Shows as "Listening X guilds"
+
 });
 
