74ea3164443e4e615dbec6ec65c0bbe899fd5ad9
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-02 17:44:37 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -7,7 +7,7 @@ module.exports = {
         // Main Help Embed
         const embed = new MessageEmbed()
             .setTitle("Mytho Help")
-            .setDescription("<:mytho:1230996358081544222> Mytho, the Discord safeguard bot, is operated by Reduce and created by `ogqvnrvx`. For a comprehensive list of commands, please refer to our [documentation](https://qvnsecret.github.io/mytho/).")
+            .setDescription("<:home:1233852688857563166> Mytho, the Discord safeguard bot, is operated by Reduce and created by `ogqvnrvx`. For a comprehensive list of commands, please refer to our [documentation](https://qvnsecret.github.io/mytho/).")
             .setColor("#2b2d31")
             .setFooter("Mytho™ - 2024/2025")
             .setTimestamp();
