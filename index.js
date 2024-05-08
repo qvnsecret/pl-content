e1ce9bc34395bf9b2a612f43629c3df9e1f0d155
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-08 17:12:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,39 @@ const app = express();
 const chalk = require("chalk");
 const os = require('os');
 
+// Serve the home route
 app.get('/', (req, res) => {
     res.send('Hello Express app!')
 });
 
+// Serve the ping route
 app.use('/ping', (req, res) => {
     res.send(new Date());
 });
 
+// Start the Express server
 app.listen(3000, () => {
     console.log(chalk.greenBright.bold('The server is now running and listening on port 3000.'));
 });
 
+// Import the Discord.js library and create a new client
 const { Client, Collection, MessageEmbed } = require("discord.js");
 const client = new Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
     allowedMentions: { parse: ['users', 'roles'], repliedUser: false },
 });
 
+// Export the client for use in other modules
 module.exports = client;
+
+// Initialize collections for commands and slash commands
 client.commands = new Collection();
 client.slashCommands = new Collection();
-client.config = require("./config.json");
 
+// Import and initialize the command handler
 require("./handler")(client);
 
+// Listen for messageCreate event and handle bot mentions
 client.on('messageCreate', message => {
     // Check if the bot is specifically mentioned and ignore messages from bots (including itself)
     if (message.mentions.users.has(client.user.id) && !message.mentions.everyone && !message.author.bot) {
@@ -41,26 +49,7 @@ client.on('messageCreate', message => {
     }
 });
 
-const channelId = '1230623201101484134'; // Replace 'YOUR_CHANNEL_ID' with the ID of your desired channel
-
-client.once('ready', () => {
-    const channel = client.channels.cache.get(channelId);
-    if (!channel) return console.error(`Channel with ID ${channelId} not found.`);
-
-    // Send initial embed
-    channel.send({ embeds: [createEmbed()] }).then(sentMessage => {
-        // Update the embed every 5 seconds
-        setInterval(() => {
-            sentMessage.edit({ embeds: [createEmbed()] }).catch(console.error);
-        }, 5000); // Update every 5000ms (5 seconds)
-    }).catch(console.error);
-});
-
+// Log in the client using the token from environment variable or config.json
 client.login(process.env.token || client.config.token).catch(err => {
     console.error(err.message);
 });
-
-// Function to create the embed
-function createEmbed() {
-    // Embed content goes here
-}
