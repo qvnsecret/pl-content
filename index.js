e0d6c396fb51d77ea586ae2da75a31af612ba727
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-08 17:28:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ app.listen(3000, () => {
     console.log(chalk.greenBright.bold('The server is now running and listening on port 3000.'));
 });
 
-const { Client, Collection, MessageEmbed } = require("discord.js");
+const { Client, Collection, MessageEmbed, GatewayIntentBits, Partials } = require("discord.js");
 const client = new Client({
-    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
-    allowedMentions: { parse: ['users', 'roles'], repliedUser: false },
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent, // Required to receive messages in Guilds
+        GatewayIntentBits.GuildMembers
+    ],
+    allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
+    partials: [Partials.Message, Partials.Channel, Partials.GuildMember] // In case you need partials
 });
 
 module.exports = client;
@@ -29,11 +35,10 @@ client.config = require("./config.json");
 require("./handler")(client);
 
 client.on('messageCreate', message => {
-    // Check if the bot is specifically mentioned and ignore messages from bots (including itself)
-    if (message.mentions.users.has(client.user.id) && !message.mentions.everyone && !message.author.bot) {
-        // Send an embed in response to the mention
+    // Check if the bot was specifically mentioned and if the message was not sent by a bot
+    if (message.mentions.has(client.user.id) && !message.mentions.everyone && !message.author.bot) {
         const embed = new MessageEmbed()
-            .setColor("#2b2d31")  // Dark gray color
+            .setColor("#2b2d31") // Dark gray color
             .setDescription("My prefix is `.`\nIf you want more info, type `.help` to see all the commands available.");
 
         message.reply({ embeds: [embed] });
@@ -41,5 +46,5 @@ client.on('messageCreate', message => {
 });
 
 client.login(process.env.token || client.config.token).catch(err => {
-    console.error(err.message);
+    console.error('Failed to login to Discord:', err.message);
 });
