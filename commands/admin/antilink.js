435a506681d10b15dc4ac967591b6d921d5b980f
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-08 21:25:05 +0200

    Update antilink.js

--- commands/admin/antilink.js
+++ commands/admin/antilink.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "antilink",
-    description: "Blocks or removes links and sends a warning message.",
+    description: "Configures the anti-link settings.",
     run: async (client, message, args) => {
         const permission = message.member.permissions.has("MANAGE_MESSAGES");
 
@@ -16,18 +16,18 @@ module.exports = {
         // Check if the message content contains a link
         const linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
         if (linkRegex.test(message.content)) {
-            // Remove the link and send a message with a warning
-            const noLinkMessage = message.content.replace(linkRegex, '**[LINK REMOVED]**');
-            message.reply({
-                content: `<@${message.author.id}>, you can't send links here.`,
-                allowedMentions: { parse: ['users'], repliedUser: true },
-            }).then(() => {
-                message.edit({ content: noLinkMessage }).catch(console.error);
-            });
+            // Remove the message and send a message with a warning
+            message.delete().catch(console.error);
+            const embed = new MessageEmbed()
+                .setColor("#2b2d31")
+                .setDescription(`<@${message.author.id}>, you can't send links here.`)
+                .setTimestamp()
+                .setFooter(`Anti-link settings configured by ${message.author.tag}`);
+            message.channel.send({ embeds: [embed] }).catch(err => console.log(`I couldn't send the message: ${err.message}`));
         } else {
             const embed = new MessageEmbed()
                 .setColor("#2b2d31")
-                .setDescription("**No link found in the message.**");
+                .setDescription("**Anti-link settings configured.**");
             message.reply({ embeds: [embed], ephemeral: true }).catch(err => console.log(`I couldn't reply to the message: ${err.message}`));
         }
     },
