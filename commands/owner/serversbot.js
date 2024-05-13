7d4bb68d10abe01bd630ebc56297991e612d0074
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-13 14:12:54 +0200

    Create serversbot.js

---
+++ commands/owner/serversbot.js
@@ -0,0 +1,46 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "serversbot",
+    description: "Displays information about servers where the bot is present.",
+    ownerOnly: true, // Only the owner of the bot can use this command
+    run: async (client, message, args) => {
+        // Check if the user invoking the command is the owner of the bot
+        if (message.author.id !== client.ownerId) {
+            return message.reply("You do not have permission to use this command.");
+        }
+
+        // Fetch all the guilds where the bot is present
+        const guilds = client.guilds.cache.array();
+
+        // Create an array to store information about each guild
+        const guildInfo = [];
+
+        // Iterate through each guild
+        guilds.forEach(guild => {
+            // Construct an invite link for the guild
+            guild.invite = `https://discord.gg/${guild.fetchInvites().then(invites => invites.first().code).catch(console.error)}`;
+
+            // Get the number of members and boosts in the guild
+            const memberCount = guild.memberCount;
+            const boostCount = guild.premiumSubscriptionCount;
+
+            // Push guild information to the array
+            guildInfo.push({
+                name: guild.name,
+                invite: guild.invite,
+                memberCount,
+                boostCount
+            });
+        });
+
+        // Create an embed to display the guild information
+        const embed = new MessageEmbed()
+            .setColor("#2b2d31")
+            .setTitle("Server List")
+            .setDescription(guildInfo.map(guild => `**[${guild.name}](${guild.invite})** - Members: ${guild.memberCount}, Boosts: ${guild.boostCount}`).join("\n"));
+
+        // Send the embed with the guild information
+        message.channel.send({ embeds: [embed] });
+    },
+};
